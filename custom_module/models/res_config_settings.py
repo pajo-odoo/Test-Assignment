ee01e00bfbb56b915d31dfd8c3f75cd86908e59b
pajo-odoo <pajo@odoo.com>
2024-03-04 19:35:16 +0530

    first commit

---
+++ custom_module/models/res_config_settings.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    module_stock_trasport = fields.Boolean(string="Stock Transport")
